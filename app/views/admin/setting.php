1ca3dfff1fee3b7c05ded16fd1ce5a243da611a2
nneliifeanyi <stanvicbest@gmail.com>
2023-05-29 12:17:31 +0100

    still demo

--- app/views/admin/setting.php
+++ app/views/admin/setting.php
@@ -5,8 +5,34 @@
 
             <?php include APPROOT . '/views/admin/inc.php' ?>
 
-            <div class="col-lg-9 text-center">
-                <h2>My Profile</h2>
+            <div class="col-lg-9">
+                <h2 class="text-center">My Profile</h2>
+                <div class="row">
+
+                    <div class="col mx-auto bg-light rounded">
+                        <div class="col-4 px-4 bg-primary mx-auto my-3">
+                        <img class="card-img rounded-circle" src="" style="height: 150px;">
+                        </div>
+                        <div class="px-5">
+                            <label>Name:</label>
+                            <input readonly class="form-control form-control-lg"  value="sky">
+                        
+                            <label>Phone</label>
+                            <input readonly class="form-control form-control-lg" value="sky">
+                        
+                            <label>Email:</label>
+                            <input readonly class="form-control form-control-lg" value="sky">
+                        
+                            <label>Business Address:</label>
+                            <input readonly class="form-control form-control-lg"  value="sky">
+                        
+                            <label>Change Password:</label>
+                            <input readonly class="form-control form-control-lg" value="sky">
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
