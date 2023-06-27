07fea5c49886d9405b9083dbfc326e440061aa23
nneliifeanyi <stanvicbest@gmail.com>
2023-06-27 11:57:54 +0100

    modified a litle

--- app/views/inc/nav.php
+++ app/views/inc/nav.php
@@ -7,7 +7,7 @@
                             <i class="fa fa-fw fa-chevron-circle-down mt-1"></i>
                         </a>
                         <ul class="collapse show list-unstyled pl-3">
-                            <li><a class="text-decoration-none" href="<?=URLROOT?>/phones/iphones">iPhones</a></li>
+                            <li><a class="text-decoration-none" href="https://dematrixphones.com" target="_blank">iPhones</a></li>
                             <li><a class="text-decoration-none" href="<?=URLROOT?>/phones/android">Android Phones</a></li>
                             <li><a class="text-decoration-none" href="<?=URLROOT?>/phones/others">Other Phones</a></li>
                         </ul>
@@ -23,6 +23,16 @@
                             <li><a class="text-decoration-none fs-6" href="<?=URLROOT?>/accessories/memories">Memory Cards</a></li>
                             <li><a class="text-decoration-none fs-6" href="<?=URLROOT?>/accessories/power">Power Bank</a></li>
                             <li><a class="text-decoration-none fs-6" href="<?=URLROOT?>/accessories/battery">Batteries</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="pb-2">
+                        <a class="collapsed d-flex justify-content-between h3 text-decoration-none" href="#">
+                            Own a website
+                            <i class="fa fa-fw fa-chevron-circle-down mt-1"></i>
+                        </a>
+                        <ul class="collapse show list-unstyled pl-3">
+                            <li><a class="text-decoration-none fs-5 text-success" href="https://wa.me/8122321931?text=I%20need%20a%20website%20of%20my%20own%20pls..">let's do it</a></li>
                         </ul>
                     </li>
                 </ul>
